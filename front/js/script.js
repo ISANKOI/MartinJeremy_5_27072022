3e06b39fe6567e188dcb0e4311f99b1c32f362cb
Jérémy Martin <martinjeremy147@gmail.com>
2022-07-27 19:18:37 +0200

    JS affichage canapes

--- front/js/script.js
+++ front/js/script.js
@@ -1,20 +1,34 @@
+function canape_to_html(data){
+    return`<a href="./product.html?id=${data._id}">
+    <article>
+      <img src="${data.imageUrl}" alt=${data.altTxt}">
+      <h3 class="productName">${data.name}</h3>
+      <p class="productDescription">${data.description}</p>
+    </article>
+  </a>`
+}
+
+function afficheDatas(canapes) {
+    const items = document.getElementById("items")
+    var result = "" 
+    for(let c in canapes){
+        let canape = canapes[c]
+        result += canape_to_html(canape)
+    }
+    
+    items.innerHTML = result          
+}
 fetch("http://localhost:3000/api/Products")
     .then(function(res) {
-        if (res.ok) {
         return res.json();
-        }
+        
     })
     .then(function(data) {
         console.log(data);
+        afficheDatas(data)
     })
       .catch(function(err) {
         // Une erreur est survenue
     });
-        // Appel de ta fonction d'affichage
-        function afficheDatas(data) {
-            data.map(function(produit, index) {
-             document
-                .getElementById("items")
-                items.appendChild       
-            });
-          }
+    
+
