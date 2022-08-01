bed404637ca09b10408041e70891b9d2117eae10
Jérémy Martin <martinjeremy147@gmail.com>
2022-08-01 16:21:46 +0200

    Ajout selection couleurs

--- front/js/product.js
+++ front/js/product.js
@@ -3,29 +3,37 @@ var str = window.location.href;
 var url = new URL(str)
 var idProduct = url.searchParams.get("id")
     console.log(idProduct)
-var article = ""
 
-    // Affichage caracteristique du canape
-function afficheDatas(canape) {
+    // Affichage caracteristiques du canape
+function afficheDatas(data) {
     const productImg = document.createElement("img")
     document.querySelector(".item__img").appendChild(productImg)
-    productImg.src = canape.imageUrl
-    productImg.alt = canape.altTxt
+    productImg.src = data.imageUrl
+    productImg.alt = data.altTxt
 
     const productTitle = document.querySelector("#title")
-    productTitle.innerHTML = canape.name
+    productTitle.innerHTML = data.name
 
     const productPrice = document.querySelector("#price")
-    productPrice.innerHTML = canape.price
+    productPrice.innerHTML = data.price
 
     const productDesc = document.querySelector("#description")
-    productDesc.innerHTML = canape.description
+    productDesc.innerHTML = data.description
 
-    // LES BOUTONS A FAIRE
-
-      
+    // Choix de la couleur
+    for(let c in data.colors) {
+        let productColors = document.createElement("option");
+        document.querySelector("#colors").appendChild(productColors);
+        productColors.value = data.colors[c];
+        productColors.innerHTML = data.colors[c];
+    } 
+    
+          
 }
-// Recuperation caracteristique canape
+   /*   //Ajouter au panier
+     btn_AddToCard.addEventlistener("click",)*/
+      
+// Recuperation caracteristiques canape
 fetch("http://localhost:3000/api/Products/" + idProduct)
     .then(function(res) {
         return res.json();
@@ -34,6 +42,7 @@ fetch("http://localhost:3000/api/Products/" + idProduct)
     .then(function(data) {
         console.log(data);
         afficheDatas(data)
+        
     })
       .catch(function(err) {
         // Une erreur est survenue
