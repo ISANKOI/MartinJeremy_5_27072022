ebff8fa0fabe217babc7d4785aeba646718b5ab1
Jérémy Martin <martinjeremy147@gmail.com>
2022-08-04 16:34:27 +0200

    ajout localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,49 +1,67 @@
 // Recuperation ID canape
 var str = window.location.href;
-var url = new URL(str)
-var idProduct = url.searchParams.get("id")
-    console.log(idProduct)
+var url = new URL(str);
+var productId = url.searchParams.get("id");
+console.log(productId);
+const productQuant = document.querySelector("#quantity");
+const productColor = document.querySelector("#colors");
 
-    // Affichage caracteristiques du canape
+// Affichage caracteristiques du canape
 function afficheDatas(data) {
-    const productImg = document.createElement("img")
-    document.querySelector(".item__img").appendChild(productImg)
-    productImg.src = data.imageUrl
-    productImg.alt = data.altTxt
+  const productImg = document.createElement("img");
+  document.querySelector(".item__img").appendChild(productImg);
+  productImg.src = data.imageUrl;
+  productImg.alt = data.altTxt;
 
-    const productTitle = document.querySelector("#title")
-    productTitle.innerHTML = data.name
+  const productTitle = document.querySelector("#title");
+  productTitle.innerHTML = data.name;
 
-    const productPrice = document.querySelector("#price")
-    productPrice.innerHTML = data.price
+  const productPrice = document.querySelector("#price");
+  productPrice.innerHTML = data.price;
 
-    const productDesc = document.querySelector("#description")
-    productDesc.innerHTML = data.description
+  const productDesc = document.querySelector("#description");
+  productDesc.innerHTML = data.description;
 
-    // Choix de la couleur
-    for(let c in data.colors) {
-        let productColors = document.createElement("option");
-        document.querySelector("#colors").appendChild(productColors);
-        productColors.value = data.colors[c];
-        productColors.innerHTML = data.colors[c];
-    } 
-    
-          
+  // Choix de la couleur
+  for (let c in data.colors) {
+    let productColors = document.createElement("option");
+    document.querySelector("#colors").appendChild(productColors);
+    productColors.value = data.colors[c];
+    productColors.innerHTML = data.colors[c];
+  }
 }
-   /*   //Ajouter au panier
-     btn_AddToCard.addEventlistener("click",)*/
-      
+
+//Ajouter au panier
+
+const bouton = document.getElementById("addToCart");
+console.log(bouton);
+bouton.addEventListener("click", (event) => {
+  const colors = productColor.value;
+  console.log(colors);
+  const quantity = productQuant.value;
+  if (quantity == 0 || quantity > 100) {
+    alert("veuillez selectionner une quantité (1-100)");
+    return;
+  }
+  if (colors == "") {
+    alert("veuillez selectionner une couleur");
+    return;
+  }
+  //cart.add(productId, colors, quantity)
+  const productChoice = [productId, colors, quantity];
+  localStorage.setItem(productId, JSON.stringify(productChoice));
+  window.location.href = "cart.html";
+});
+
 // Recuperation caracteristiques canape
-fetch("http://localhost:3000/api/Products/" + idProduct)
-    .then(function(res) {
-        return res.json();
-        
-    })
-    .then(function(data) {
-        console.log(data);
-        afficheDatas(data)
-        
-    })
-      .catch(function(err) {
-        // Une erreur est survenue
-    });
+fetch("http://localhost:3000/api/Products/" + productId)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    afficheDatas(data);
+  })
+  .catch(function (err) {
+    // Une erreur est survenue
+  });
